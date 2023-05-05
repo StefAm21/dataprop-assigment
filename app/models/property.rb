3e8a6e58aa2b7ac5ec70eff5661e2e44b0043711
stefano amodei <samodei@udesa.edu.ar>
2023-05-05 05:40:54 -0300

    Basic property rendering view and form

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,13 @@
 class Property < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  enum property_type: { arriendo: 0, compra: 1 }
+  validates :property_type, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :comuna, presence: true
+  validates :area, presence: true, numericality: { greater_than: 0 }
+  validates :rooms, presence: true, numericality: { greater_than: 0 }
+  validates :bathrooms, presence: true, numericality: { greater_than: 0 }
+  has_many_attached :photos
+  validates :photos, presence: true
 end
